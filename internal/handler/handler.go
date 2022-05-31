4f4fce8a0dc57a4f50413735ff90718865435562
amirhnajafiz <najafizadeh21@gmail.com>
2022-05-31 11:13:51 +0430

    feat: add handler methods

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -4,8 +4,24 @@ import "github.com/gofiber/fiber/v2"
 
 func Register(app *fiber.App) {
 	// creating our endpoints
-	app.Get("/")
-	app.Post("/")
-	app.Put("/update")
-	app.Delete("/delete")
+	app.Get("/", index)               // home
+	app.Post("/", postHandler)        // post handler
+	app.Put("/update", putHandler)    // update post handler
+	app.Delete("/delete", delHandler) // delete post handler
+}
+
+func index(c *fiber.Ctx) error {
+	return c.SendString("hello world")
+}
+
+func postHandler(c *fiber.Ctx) error {
+	return c.SendString("hello world")
+}
+
+func putHandler(c *fiber.Ctx) error {
+	return c.SendString("hello world")
+}
+
+func delHandler(c *fiber.Ctx) error {
+	return c.SendString("hello world")
 }
