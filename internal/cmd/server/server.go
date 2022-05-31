700029b0da5b3b9dc260868a840c40fffcf84436
amirhnajafiz <najafizadeh21@gmail.com>
2022-05-31 11:06:43 +0430

    feat: comments for server pkg

--- internal/cmd/server/server.go
+++ internal/cmd/server/server.go
@@ -8,12 +8,15 @@ import (
 )
 
 func New() {
+	// creating a new fiber
 	app := fiber.New()
 
+	// getting the port from env variables
 	port := os.Getenv("port")
 	if port == "" {
-		port = "8080"
+		port = "8080" // default port is 8080
 	}
 
+	// starting our server
 	log.Fatalln(app.Listen(":" + port))
 }
