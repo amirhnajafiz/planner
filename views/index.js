1c188347684a783cdbd3faaccdbdbf5e508f9105
amirhnajafiz <najafizadeh21@gmail.com>
2022-06-05 15:46:23 +0430

    index updated

--- views/index.js
+++ views/index.js
@@ -1,7 +1,18 @@
-function updateDb() {
-
+function updateDb(item) {
+    let input = document.getElementById(item)
+    let newItem = input.value
+    fetch(`/update?olditem=${item}&newitem=${newItem}`, {method: "PUT"}).then(res =>{
+        if (res.status === 200){
+            alert("Database updated")
+            window.location.pathname = "/"
+        }
+    })
 }
 
-function removeFromDb() {
-
+function removeFromDb(item) {
+    fetch(`/delete?item=${item}`, {method: "Delete"}).then(res =>{
+        if (res.status === 200){
+            window.location.pathname = "/"
+        }
+    })
 }
