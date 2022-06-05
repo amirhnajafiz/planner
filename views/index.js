07c658d188d6e7612c59823a434d0648b6ff00ae
amirhnajafiz <najafizadeh21@gmail.com>
2022-06-05 16:04:13 +0430

    update: js

--- views/index.js
+++ views/index.js
@@ -1,18 +1,30 @@
+// this method updates an item
 function updateDb(item) {
     let input = document.getElementById(item)
     let newItem = input.value
-    fetch(`/update?olditem=${item}&newitem=${newItem}`, {method: "PUT"}).then(res =>{
-        if (res.status === 200){
-            alert("Database updated")
-            window.location.pathname = "/"
-        }
-    })
+
+    let url = `/update?olditem=${item}&newitem=${newItem}`
+
+    fetch(url, {method: "PUT"})
+        .then(res => {
+            if (res.status === 200) {
+                alert("Item removed")
+
+                window.location.pathname = "/"
+            }
+        })
+        .catch(e => console.error(e))
 }
 
+// this method removes an item
 function removeFromDb(item) {
-    fetch(`/delete?item=${item}`, {method: "Delete"}).then(res =>{
-        if (res.status === 200){
-            window.location.pathname = "/"
-        }
-    })
+    let url = `/delete?item=${item}`
+
+    fetch(url, {method: "Delete"})
+        .then(res => {
+            if (res.status === 200) {
+                window.location.pathname = "/"
+            }
+        })
+        .catch(e => console.error(e))
 }
