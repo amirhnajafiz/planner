e08bfca0db6bec8f95f634546604386b280fcaef
amirhnajafiz <najafizadeh21@gmail.com>
2022-07-04 17:23:32 +0430

    add: flag

--- setup.go
+++ setup.go
@@ -2,12 +2,18 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/amirhnajafiz/planner/internal/db"
 )
 
 func main() {
-	err := db.Migrate(true)
+	flag := false
+	if os.Args[1] == "migrate" {
+		flag = true
+	}
+
+	err := db.Migrate(flag)
 	if err != nil {
 		log.Fatal(err)
 	}
